0e8781ffd29a3d547ae0fe9638ea5a7f0a79013e
Kamil Kamiński <kamilkaminski39@gmail.com>
2024-03-21 11:23:00 +0100

    feat: Scroll to top after route change

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,10 @@ import { routes } from './routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  }
 })
 
 router.beforeEach((to) => {
